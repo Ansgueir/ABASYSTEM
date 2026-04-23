932d40da3cb766dc9e4913d8039d55d33979cc9d
Andyf <andyf@abasystem.com>
2026-04-23 17:57:39 -0400

    chore: add user lister script

---
+++ src/scripts/list-all.ts
@@ -0,0 +1,15 @@
+
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+async function main() {
+  const users = await prisma.user.findMany({
+    select: { id: true, email: true, role: true }
+  })
+  console.log('--- ALL USERS IN DB ---')
+  console.log(JSON.stringify(users, null, 2))
+  console.log('--- END ---')
+}
+
+main().finally(() => prisma.$disconnect())
